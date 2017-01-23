825bf8648562012d5758daa228d8b8bfeaf46517
nebulaM <nebulam12@gmail.com>
2017-01-22 22:22:58 -0800

    delete unnecessary cmds

--- init_setup.sh
+++ init_setup.sh
@@ -17,7 +17,6 @@ mkdir battery-service
 mv BatteryStateServer/bleno_update/* battery-service/
 mv BatteryStateServer/gatt.js bleno/lib/hci-socket/
 
-
 #enter battery-service directory
 cd battery-service
 npm install node-hid
@@ -29,35 +28,27 @@ npm install
 
 cd ..
 
-
 #setup i2c general(one time setup)
-#add i2c-bcm2708
 # > to overrite, >> to append
-echo "i2c-bcm2708" > myi2c
-sudo cat /etc/modules ./myi2c >> /etc/modules
-rm myi2c
+sudo echo "i2c-bcm2708" >> /etc/modules && echo "appended i2c-bcm2708 to /etc/modules..."
 sudo modprobe i2c-bcm2708
-#check device is up
+#check that device is up
 lsmod
 
-echo "dtparam=i2c_arm=on" > myi2cboot
-sudo cat /boot/config.txt ./myi2cboot >> /boot/config.txt
-rm myi2cboot
-
-sudo cat ~/.bashrc ./init/bashrc >> ~/.bashrc
+sudo echo "dtparam=i2c_arm=on" >> /boot/config.txt && echo "appended dtparam=i2c_arm=on to /boot/config.txt..."
+#bashrc setup
+sudo cat ~/.bashrc ./init/bashrc >> ~/.bashrc && echo "setup job onReboot..."
 
 #important,install a crontab on reboot to prevent from a strange "command disallowed error"
 crontab -l > mycron
 echo "@reboot sudo node /home/pi/Documents/battery-service/main.js" >> mycron
-crontab mycron
+crontab mycron && echo "installed crontab..."
 rm mycron
-#auto start a terminal on boot
-#add this to end of file:
-echo "@lxterminal" > myAuto
-sudo cat ~/.config/lxsession/LXDE-pi/autostart ./myAuto >> ~/.config/lxsession/LXDE-pi/autostart
-rm myAuto
 
-echo "finsihed setup"
+#auto start a terminal on boot
+sudo echo "@lxterminal" >> ~/.config/lxsession/LXDE-pi/autostart && echo "setup autostart a terminal onReoot..."
+
+echo "finsihed all setup..."
 
 
 
