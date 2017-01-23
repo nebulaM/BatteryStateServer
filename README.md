d0aec8c0d2222ad87bfd149c16eead17b8cb7ea8
nebulaM <nebulaM12@gmail.com>
2017-01-22 23:54:39 -0800

    update init_setup script

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This repo contains two programs that communicate with [an Mobile App](https://github.com/nebulaM/BatteryStateBLE).
 
+For a new Raspberry Pi that can be used as a Bluetooth Server, use [raspbain](https://www.raspberrypi.org/downloads/raspbian/) OS and run the one time setup script **init_setup.sh** as root(important)
+```sh
+chmod 777 ./init_setup.sh
+sudo ./init_setup.sh
+```
+
 The [Bluetooth Server](https://github.com/nebulaM/BatteryStateServer/tree/master/bleno_update) collects battery data via an I2C chip and then sends this data to the phone app.
 
 The phone app then sends the battery data to a [Network Server](https://github.com/nebulaM/BatteryStateServer/tree/master/network_server) which is used to track multiple users' battery status.
--- init_setup.sh
+++ init_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+DOCDIR=/home/pi/Documents
 echo "config bluetooth..."
 sudo systemctl stop bluetooth
 sudo systemctl disable bluetooth
@@ -7,15 +7,16 @@ sudo hciconfig hci0 up
 
 echo "resolve dependences..."
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install npm nodejs bluetooth bluez libbluetooth-dev libudev-dev libusb-1.0-0-dev
-
-cd ~/Documents
+sudo apt-get install -y npm 
+sudo apt-get install -y nodejs
+sudo apt-get install -y bluetooth bluez libbluetooth-dev libudev-dev libusb-1.0-0-dev git
+cd $DOCDIR
 git clone https://github.com/nebulaM/bleno
 mv bleno bleno-master
 git clone https://github.com/nebulaM/BatteryStateServer
 mkdir battery-service
 mv BatteryStateServer/bleno_update/* battery-service/
-mv BatteryStateServer/gatt.js bleno/lib/hci-socket/
+mv BatteryStateServer/gatt.js bleno-master/lib/hci-socket/
 
 #enter battery-service directory
 cd battery-service
@@ -26,7 +27,7 @@ npm install i2c
 cd ../bleno-master
 npm install
 
-cd ..
+cd ../BatteryStateServer
 
 #setup i2c general(one time setup)
 # > to overrite, >> to append
@@ -37,7 +38,7 @@ lsmod
 
 sudo echo "dtparam=i2c_arm=on" >> /boot/config.txt && echo "appended dtparam=i2c_arm=on to /boot/config.txt..."
 #bashrc setup
-sudo cat ~/.bashrc ./init/bashrc >> ~/.bashrc && echo "setup job onReboot..."
+sudo cat ./init/bashrc >> ~/.bashrc && echo "setup job onReboot..."
 
 #important,install a crontab on reboot to prevent from a strange "command disallowed error"
 crontab -l > mycron
@@ -49,6 +50,8 @@ rm mycron
 sudo echo "@lxterminal" >> ~/.config/lxsession/LXDE-pi/autostart && echo "setup autostart a terminal onReoot..."
 
 echo "finsihed all setup..."
+
+sudo reboot now
 
 
 
