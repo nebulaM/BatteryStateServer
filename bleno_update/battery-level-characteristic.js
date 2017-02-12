bff4094ef7ccb89ccfcd3c90648e6f780e7e938b
nebulaM <nebulaM12@gmail.com>
2017-02-12 11:09:49 -0800

    delete redundant code

--- bleno_update/battery-level-characteristic.js
+++ bleno_update/battery-level-characteristic.js
@@ -19,10 +19,9 @@ const fs = require('fs');
 const path ='/home/pi/Documents/ipAddr.txt';
 var hasIP=false;
 if (!fs.existsSync(path)) {
-	hasIP=false;
-
+  hasIP=false;
 }else{
-	hasIP=true;
+  hasIP=true;
 }
 
 var BatteryLevelCharacteristic = function() {
@@ -44,146 +43,123 @@ function readIP(){
   var ip=fs.readFileSync(path, 'ascii');
   ip=ip.trim();
   ip=ip.split(".");
-  console.log(ip);
   var byteArray = new Uint8Array(ip.length);
   for(var i=0;i<ip.length;++i){
-	  for(var j=0;j<ip[i].length;++j){
+    for(var j=0;j<ip[i].length;++j){
       switch(ip[i].length-1-j){
       case 0:
-      	byteArray[i]+=(ip[i].charCodeAt(j)-48);
-      	break;
+        byteArray[i]+=(ip[i].charCodeAt(j)-48);
+        break;
       case 1:
-      	byteArray[i]+=(ip[i].charCodeAt(j)-48)*10;
-      	break;
+        byteArray[i]+=(ip[i].charCodeAt(j)-48)*10;
+        break;
       case 2:
-      	byteArray[i]+=(ip[i].charCodeAt(j)-48)*100;
-      	break;
+        byteArray[i]+=(ip[i].charCodeAt(j)-48)*100;
+        break;
       default:
-      	byteArray[i]+=(ip[i].charCodeAt(j)-48);
-      	break;
+        byteArray[i]+=(ip[i].charCodeAt(j)-48);
+        break;
       }
-	   }
+    }
   }
-  //console.log(byteArray)
   return byteArray
 }
 
 var uniqueID=[];
-var batteryLevelQueue=[];
-var batteryHealthQueue=[];
+var batteryStatusQueue=[];
 BatteryLevelCharacteristic.prototype.setI2cUpdateInterval=function(ms){
   this.i2cUpdateInterval=setInterval(function() {
-		//no ID for the I2C chip yet
-		//6 byte uniqueID
-		if(uniqueID.length!=6){
-			fuelGauge.getUniqueID((function (data) {
-			uniqueID=data;
-			console.log("@setI2cUpdateInterval uniqueID: "+uniqueID);
-		}));
-		}
-    if(batteryLevelQueue.length>20){
-      //dequeue the last item from queue, just throw that data away
-      batteryLevelQueue.shift();
-    }
-	if(batteryHealthQueue.length>20){
-      //dequeue the last item from queue, just throw that data away
-      batteryHealthQueue.shift();
-    }
-    fuelGauge.getVoltage((function(data){
-      batteryLevelQueue.push(data);
-      //console.log("@setI2cUpdateInterval put into queue: from fuel gauge, voltage is "+data);
-    }));
+  //6 byte uniqueID
+  if(uniqueID.length!=6){
+    fuelGauge.getUniqueID((function (data) {
+      uniqueID=data;
+      console.log("@setI2cUpdateInterval uniqueID: "+uniqueID);}));
+  }
+  if(batteryStatusQueue.length>20){
+    //dequeue the last item from queue, drop that data
+    batteryStatusQueue.shift();
+  }
+  
+  fuelGauge.getBatteryStatus((function(data){batteryStatusQueue.push(data);}));
 
-	fuelGauge.getCurrent((function(data){
-		console.log("from fuel gauge, current is "+data);
-	}));
-
-	fuelGauge.getHealth((function(data){
-		console.log("from fuel gauge, health is "+data);
-		batteryHealthQueue.push(data);
-	}));},ms);
+  fuelGauge.getCurrent((function(data){
+    console.log("from fuel gauge, current is "+data);}));
+  },ms);
 };
 
 BatteryLevelCharacteristic.prototype.onReadRequest = function(offset, callback) {
-	var data=[];
-	var errorCode=0;
-	if(batteryLevelQueue.length<=0){
-	  errorCode=1;
-	}
-	if(batteryHealthQueue.length<=0){
-	  errorCode=1;
-	}
-	if(uniqueID.length!=6){
-		errorCode=1;
-	}
-	data.push(errorCode);
-	data=data.concat(uniqueID);
-	data.push(parseInt(batteryLevelQueue.shift()));
-	//for now we do not have health
-	data.push(parseInt(batteryHealthQueue.shift())&0xFF);
-	if(needIP&&hasIP){
-		//data 2-5 ip addr
-		var ip=readIP();
-		for(var i=0;i<4;++i){
-			data.push(ip[i]);
-		}
-	}
-	if(errorCode==1){
-		callback(this.RESULT_SUCCESS,errorCode);
-	}else{
-		if(verbose){
-	  	console.log("@onReadRequest data to be sent is "+data);
-	  }
-  	callback(this.RESULT_SUCCESS, data);
-	}
-
+  var data=[];
+  var errorCode=0;
+  if(batteryStatusQueue.length<=0){
+    errorCode=1;
+  }
+  if(uniqueID.length!=6){
+    errorCode=1;
+  }
+  data.push(errorCode);
+  data=data.concat(uniqueID);
+  var temp=batteryStatusQueue[0];
+  data.push(parseInt((temp>>8)&0xFF));
+  data.push(parseInt(temp&0xFF));
+  if(needIP&&hasIP){
+    //data 2-5 ip addr
+    var ip=readIP();
+    for(var i=0;i<4;++i){
+      data.push(ip[i]);
+    }
+  }
+  if(errorCode==1){
+    callback(this.RESULT_SUCCESS,errorCode);
+  }else{
+    if(verbose){
+      console.log("@onReadRequest data to be sent is "+data);
+    }
+    callback(this.RESULT_SUCCESS, data);
+  }
 };
 
 BatteryLevelCharacteristic.prototype.onSubscribe = function(maxValueSize, callback) {
-	if(verbose){
-		console.log("@onSubscribe Device  subscribed");
-	}
-	if(this.i2cUpdateInterval!=null){
-	  clearInterval(this.i2cUpdateInterval);
-	}
-	//data array is errorCode + uniqueID + battery level + health (+ IP)
-	//							1 byte			6 bytes				1 byte				1 byte	4 bytes
-	//errorCode: 0-no error; 1-error
+  if(verbose){
+    console.log("@onSubscribe Device  subscribed");
+  }
+  if(this.i2cUpdateInterval!=null){
+    clearInterval(this.i2cUpdateInterval);
+  }
+  //data array is errorCode + uniqueID + level + health (+ IP)
+  //				1 byte		6 bytes	   1 byte	1 byte	4 bytes
+  //errorCode: 0-no error; 1-error
+  this.setI2cUpdateInterval(300);
+  this.interval=setInterval(function() {
+    var data=[];
+    var errorCode=0;
+    if(batteryStatusQueue.length<=0){
+      errorCode=1;
+    }
+    data.push(errorCode);
+    data=data.concat(uniqueID);
+    var temp=batteryStatusQueue.shift();
+    data.push(parseInt((temp>>8)&0xFF));
+    data.push(parseInt(temp&0xFF));
 
-	this.setI2cUpdateInterval(300);
-	this.interval=setInterval(function() {
-	var data=[];
-	var errorCode=0;
-	if(batteryLevelQueue.length<=0){
-	  errorCode=1;
-	}
-	if(batteryHealthQueue.length<=0){
-	  errorCode=1;
-	}
-	data.push(errorCode);
-	data=data.concat(uniqueID);
-	data.push(parseInt(batteryLevelQueue.shift()));
-	data.push(parseInt(batteryHealthQueue.shift())&0xFF);
-
-	//poll sensor or get value or something
-	if(errorCode==1){
-		callback(errorCode);
-	}else{
-		if(verbose){
-			console.log("@onSubscribe data to be sent is "+data);
-		}
-  	callback(data);
-	}}, 1000);
+    if(errorCode==1){
+      callback(errorCode);
+    }else{
+      if(verbose){
+        console.log("@onSubscribe data to be sent is "+data);
+      }
+      callback(data);
+    }
+  }, 1000);
 };
 
 BatteryLevelCharacteristic.prototype.onUnsubscribe = function(){
   if(verbose){
     console.log("@onUnsubscribe Device unsubscribed");
-	//console.log("@onUnsubscribe slower battery data update rate from 300ms to 10s");
+    //console.log("@onUnsubscribe slower battery data update rate from 300ms to 10s");
   }
   clearInterval(this.interval);
   if(this.i2cUpdateInterval!=null){
-	  clearInterval(this.i2cUpdateInterval);
+    clearInterval(this.i2cUpdateInterval);
   }
   //this.setI2cUpdateInterval(10000);
 };
--- bleno_update/max17205.js
+++ bleno_update/max17205.js
@@ -1,159 +1,65 @@
 var i2c = require('i2c');
 //0x6C on datasheet
-MAX17205.DEFAULT_ADDRESS =0x36;
+MAX17205.LOWER_PAGE =0x36;
 //0x16 on datasheet
-MAX17205.UPPER_PAGE_ADDRESS=0x0b;
+MAX17205.UPPER_PAGE=0x0b;
 
 function MAX17205(device, address) {
   this.device = device || '/dev/i2c-1';
-  this.address = address || MAX17205.DEFAULT_ADDRESS;
+  this.address = address || MAX17205.LOWER_PAGE;
 }
 
-MAX17205.nRSense=0x1CF
-//1BC-1BF contains a 64-bit unique number for this chip
-MAX17205.nROMID0=0x1BC;
-MAX17205.nDesignCap=0x1B3;
-MAX17205.nPackConfig=0x1B5;
-MAX17205.nNVConfig=0x1B8;
-
 MAX17205.prototype.initialize = function() {
-  this.i2cdev = new I2cDev(this.address, {device : this.device});
-  this.i2cUpperPage=new I2cDev(MAX17205.UPPER_PAGE_ADDRESS,{device: this.device});
-  
-  /*this.i2cUpperPage.readBytes.call(this.i2cUpperPage,MAX17205.nRSense,2, (function(){
-	
-	return (function(err, buffer) {
-	  if (err){
-       
-	   //callback(err);
-     }else{
-       var data=(buffer.readUInt8(1)<<8)+buffer.readUInt8(0);
-       console.log("nRsense is "+data);
-	   if(data!=500){
-		   console.log("will change sense register to 0x01F4" );
-		   console.log(this.constructor);
-		   this.writeBytes(MAX17205.nRSense,[0xF4,0x01],function(err) {if (err) return callback(err);});
-	   }
-	   
-    }
-	})
-  }).call(this.i2cUpperPage));*/
-  
-	this.i2cUpperPage.readBytes(MAX17205.nDesignCap,1,function(err, buffer){
-    if (err) console.log(err);
-	console.log("nDesignCap "+buffer.readUInt8(0));});
-	
-	this.i2cUpperPage.readBytes(MAX17205.nPackConfig,1,function(err, buffer){
-    if (err) console.log(err);
-	console.log("nPackConfig "+buffer.readUInt8(0));});
+  this.pageL = new i2c(MAX17205.LOWER_PAGE, {device : this.device});
+  this.pageH = new i2c(MAX17205.UPPER_PAGE, {device : this.device});
 };
 
-/**
- *reports the voltage of the entire cell stack in 2S to 4S configuration
- * if PackCfg.BtEn = 1, Batt is a direct measurement of the VBATT pin with up to 20.48V range
- */
-MAX17205.RepCap_ADDR=0x06;
-MAX17205.prototype.getVoltage= function (callback) {
-  this.i2cdev.readBytes(MAX17205.RepCap_ADDR, 2, function(err, buffer){
-  if (err){
-     callback(err);
-  }else{
-  	var data=(buffer.readUInt8(1))&0xff
-    callback(data);
-  }
+
+MAX17205.RepSoc=0x06;
+MAX17205.prototype.getBatteryStatus= function (callback) {
+  this.pageL.readBytes(MAX17205.RepSoc, 4, function(err, buffer){
+    if (err){callback(err);}
+    else{
+      //16-8 bit: level, 7-0 bit: health
+      var data=((buffer.readUInt8(1)<<8)&0xff00)+((buffer.readUInt8(3))&0xff)
+      callback(data);
+    }
   });
 };
 
 //Current Register (00Ah)
-MAX17205.CURRENT_REG_ADDR=0x0A;
+MAX17205.CURRENT_REG=0x0A;
 MAX17205.prototype.getCurrent= function (callback) {
-
-	this.i2cdev.readBytes(MAX17205.CURRENT_REG_ADDR, 2, function(err, buffer){
+    this.pageL.readBytes(MAX17205.CURRENT_REG, 2, function(err, buffer){
     if (err) return callback(err);
-	var data=(buffer.readUInt8(1)<<8)+buffer.readUInt8(0)
-	//resolution wrt sense resistance
-	//var data=data/500000
-	//console.log("data is "+ data);
-	//console.log("~data is "+ (~data));
-	data=(~data)&0xffff;
+    var data=(buffer.readUInt8(1)<<8)+buffer.readUInt8(0)
+    //resolution wrt sense resistance
+    //var data=data/500000
+    //console.log("data is "+ data);
+    //console.log("~data is "+ (~data));
+    data=(~data)&0xffff;
     callback(data);
   });
 };
 
-//Age Register(Percentage) (0x07)
-MAX17205.AGE_REG_ADDR=0x07;
-MAX17205.prototype.getHealth= function (callback) {
-
-  this.i2cdev.readBytes(MAX17205.AGE_REG_ADDR, 2, function(err, buffer){
-  if (err){
-     callback(err);
-  }else{
-  	var data=(buffer.readUInt8(1))&0xff
-    callback(data);
-  }
-  });
-};
-
+//1BC-1BF contains a 64-bit unique number for this chip
+MAX17205.nROMID0=0x1BC;
 MAX17205.prototype.getUniqueID= function (callback) {
-  this.i2cUpperPage.readBytes(MAX17205.nROMID0, 8, function(err, buffer){
-  if (err) return callback(err);
-  else{
-    var data=[];
-    //ONLY need the middle 6 bytes of data.
-    for(i=6;i>=1;i--){
-      data.push(buffer.readUInt8(i));
+  this.pageH.readBytes(MAX17205.nROMID0, 8, function(err, buffer){
+    if (err) return callback(err);
+    else{
+      var data=[];
+      //ONLY need the middle 6 bytes of data.
+      for(i=6;i>=1;i--){
+        data.push(buffer.readUInt8(i));
+      }
+      if(buffer.readUInt8(0)!=38){
+        console.log("Warnning: This ID may not be correct" );
+      }
+	  console.log("@getUniqueID: ID is "+data);
+	  callback(data);
     }
-    if(buffer.readUInt8(0)!=38){
-      console.log("Warnning: This ID may not be correct" );
-    }
-    console.log("@getUniqueID: ID is "+data);
-    callback(data);
-  }
   });
 };
-
 
 module.exports = MAX17205;
-/**
- * extends the i2c library with some extra functionality
- */
-function I2cDev(address, options) {
-  i2c.call(this, address, options);
-}
-
-I2cDev.prototype = Object.create(i2c.prototype);
-I2cDev.prototype.constructor = I2cDev;
-
-I2cDev.prototype.bitMask = function(bit, bitLength) {
-  return ((1 << bitLength) - 1) << (1 + bit - bitLength);
-};
-
-I2cDev.prototype.readBits = function(func, bit, bitLength, callback) {
-  var mask = this.bitMask(bit, bitLength);
-
-  this.readBytes(func, 1, function(err, buf) {
-    var bits = (buf[0] & mask) >> (1 + bit - bitLength);
-    callback(err, bits);
-  });
-};
-
-I2cDev.prototype.readBit = function(func, bit, bitLength, callback) {
-  this.readBits(func, bit, 1, callback);
-};
-
-I2cDev.prototype.writeBits = function(func, bit, bitLength, value) {
-  var self = this;
-  self.readBytes(func, 1, function(err, oldValue) {
-    var mask = self.bitMask(bit, bitLength);
-    var newValue = oldValue ^ ((oldValue ^ (value << bit)) & mask);
-    self.writeBytes(func, [newValue], function (err) {
-      //if (err) {
-        //throw err;
-      //}
-    });
-  });
-};
-
-I2cDev.prototype.writeBit = function(func, bit, value) {
-  this.writeBits(func, bit, 1, value);
-};
