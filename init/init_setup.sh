e15645d4021e4c83669d1665a73bfb096deca4af
nebulam <nebulam12@gmail.com>
2017-02-28 17:31:58 -0800

    mv init_setup.sh script into init dir

--- init/init_setup.sh
+++ init/init_setup.sh
@@ -2,16 +2,14 @@
 # Author: nebuleM [nebulem12@gmail.com]
 # Date:	  Jan 27th, 2017
 DOCDIR=/home/pi/Documents
+initDIR=$(pwd)
 echo "config bluetooth..."
 sudo systemctl stop bluetooth
 sudo systemctl disable bluetooth
 sudo hciconfig hci0 up
 
 echo "resolve dependences..."
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y npm 
-sudo apt-get install -y nodejs
-sudo apt-get install -y bluetooth bluez libbluetooth-dev libudev-dev libusb-1.0-0-dev git
+sudo apt-get install -y git
 cd $DOCDIR
 git clone https://github.com/nebulaM/bleno
 mv bleno bleno-master
@@ -19,6 +17,11 @@ git clone https://github.com/nebulaM/BatteryStateServer
 mkdir battery-service
 mv BatteryStateServer/bleno_update/* battery-service/
 mv BatteryStateServer/gatt.js bleno-master/lib/hci-socket/
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y bluetooth bluez libbluetooth-dev libudev-dev libusb-1.0-0-dev
 
 #enter battery-service directory
 cd battery-service
@@ -29,7 +32,7 @@ npm install i2c
 cd ../bleno-master
 npm install
 
-cd ../BatteryStateServer
+
 
 #setup i2c general(one time setup)
 # > to overrite, >> to append
@@ -40,27 +43,24 @@ lsmod
 
 sudo echo "dtparam=i2c_arm=on" >> /boot/config.txt && echo "appended dtparam=i2c_arm=on to /boot/config.txt..."
 
-python init/init_i2c.py
+#python $DOCDIR/init/init_i2c.py
 
 #bashrc setup
-sudo cat ./init/bashrc >> ~/.bashrc && echo "setup job onReboot..."
+sudo cat $initDIR/bashrc >> /home/pi/.bashrc && echo "setup job onReboot..."
 
 #important,install a crontab on reboot to prevent from a strange "command disallowed error"
-crontab -l > mycron
 echo "@reboot sudo node /home/pi/Documents/battery-service/main.js" >> mycron
 crontab mycron && echo "installed crontab..."
 rm mycron
 
 #auto start a terminal on boot
-sudo echo "@lxterminal" >> ~/.config/lxsession/LXDE-pi/autostart && echo "setup autostart a terminal onReoot..."
+sudo echo "@lxterminal" >> /home/pi/.config/lxsession/LXDE-pi/autostart && echo "setup autostart a terminal onReoot..."
 
 #essential for writting IP into a file
-cp ./writeIP.sh DOCDIR
-sudo chmod +x DOCDIR/writeIP.sh
+cp $initDIR/writeIP.sh $DOCDIR
+sudo chmod +x $DOCDIR/writeIP.sh
 
-echo "finsihed all setup..."
-
-sudo reboot now
+echo "finsihed all setup... Please reboot your device"
 
 
 
